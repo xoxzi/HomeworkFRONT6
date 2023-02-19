9b428482e70c180560994f23e45e07cf9f1657c5
xoxzi <119591980+xoxzi@users.noreply.github.com>
2023-02-19 16:45:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+//Напишите цикл for, который выводит консоль каждое второе число от 0 до 10
+
+//const array =[0,1,2,3,4,5,6,7,8,9,10]
+//const array1 =[]
+
+//for (let index = 0; index < array.length; index++) {
+ // if (index % 2 == 0) 
+  //  console.log(array[index])
+       
+//}
+ 
+//===============================================================
+
+// Напишите цикл for, который выводит в консоль  все числа от 55 до 20
+
+//for (i=55;i>20;i--)
+//{
+//console.log(i) }
+
+//===============================================================
+    
+    
+//Дан массив numbers. Вывести в консоль все числа из массива
+
+//const numbers = [3, 5, 11, 2, 8, 1, 6];
+//for (let index = 0; index < numbers.length; index++) {
+ //   console.log(numbers[index])
+    
+//}
+
+//===============================================================
+
+//Сформировать новый массив numberssquared и передать в него все элементы из массива numbers, возведенные в квадрат
+
+//const numbers = [3, 5, 11, 2, 8, 1, 6];
+
+//const numberssquared = numbers.map(num => num ** 2);
+
+//console.log(numberssquared);
+
+
+
+//===============================================================
+//Создать переменную lastelem и передать в нее последний элемент из сформированного массива numberssquared (обратиться к элементу массива по индексу)
+
+//const lastelm = numberssquared[numberssquared.length -1]
+
+//console.log(lastelm)
+
+
+//===============================================================
+
+//Дан объект user. Используя данные из объекта, сформировать строку в формате: ‘User’s name is  . He is  years old’
+
+const user =  { firstname: 'zari', 
+lastname: 'Sultanova', 
+age: 21, 
+salary:4500
+}
+ console.log("User's name is " + user.firstname +  ", she is " +user.age+ " years old")
+
+ 
